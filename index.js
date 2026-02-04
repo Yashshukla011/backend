7dca3af534cd5d0e9607aa396a32af488975fd7c
yash shukla <yashshuklas6@gmail.com>
2026-02-04 13:08:56 +0530

    g

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 require('dotenv').config()
 const app = express()
-const port = 5000
+const port = 4000
 app.get('/yash', (req, res) => {
 const arr = {
     Teacher: "Homework kyun nahi kiya?",
