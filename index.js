34a5da3a7a4719636f5c7ffb68fb54662502aef4
yash shukla <yashshuklas6@gmail.com>
2026-02-04 12:29:36 +0530

    s

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 const express = require('express')
 require('dotenv').config()
-
 const app = express()
-const port = 3000
+const port = 5000
+app.get('/yash', (req, res) => {
+const arr = {
+    Teacher: "Homework kyun nahi kiya?",
+    Student: "Sir, light nahi thi."
+  };
 
-app.get('/', (req, res) => {
-res.send([{
-    id:"yash",
-    name:"34"
-},
-{
-    id:"yash",
-    name:"34"
-}])
+  const arr1 = {
+    Teacher: "Homework kyun nahi kiya?",
+    Student: "Sir, light nahi thi."
+  };
+
+  const arr2 = {
+    Teacher: "Homework kyun nahi kiya?",
+    Student: "Sir, light nahi thi."
+  };
+
+  res.json([arr, arr1, arr2]); 
 })
 app.get('/p', (req, res) => {
-res.send("yash shukla")
+res.send("mohan")
 })
 
 app.listen(process.env.PORT, () => {
